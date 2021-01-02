b74604ddaf36402e4726051bc5fc6f1447a20aaf
Michael Walmsley <2704782+walmsles@users.noreply.github.com>
2021-01-02 15:15:48 +1100

    refactor runner.py to make all code testable

--- runner.py
+++ runner.py
@@ -1,18 +1,7 @@
-import os
-import os.path
 from bing_images.Downloader import Downloader
-
+import bing_images.utils as utils
 
 if __name__ == "__main__":
-	image_path = os.environ.get('BING_IMAGE_PATH')
-	if image_path == None:
-		print('Failed: BING_IMAGE_PATH env var not defined')
-		exit(-1)
-	if not os.path.isdir(image_path):
-		print('Failed: BING_IMAGE_PATH does not exist')
-		exit(-1)
-	
-	downloader = Downloader(image_path=image_path)
+	"""Everything in main must be imported from a unti tested file - anything in main is excluded from coverage reports"""
+	downloader = Downloader(image_path=utils.get_image_path())
 	downloader.download_all()
-		
-
