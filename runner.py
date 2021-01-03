18fda4c483e54f4dee1a3bf3e72ef8d01d51af04
walmsles <2704782+walmsles@users.noreply.github.com>
2021-01-03 23:54:58 +1100

    coverage tweaks for reporting

--- runner.py
+++ runner.py
@@ -1,7 +1,7 @@
 from bing_images.Downloader import Downloader
 import bing_images.utils as utils
 
-if __name__ == "__main__":
+if __name__ == "__main__": # pragma: no cover
 	"""Everything in main must be imported from a unti tested file - anything in main is excluded from coverage reports"""
 	downloader = Downloader(image_path=utils.get_image_path())
 	downloader.download_all()
